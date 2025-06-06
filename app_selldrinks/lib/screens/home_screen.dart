549e599ca24bd4edca975cbf5451a3c4931fcfa0
23thyu <hieungao2004@gmail.com>
2025-06-06 16:45:39 +0700

    thêm màn hình trang chủ và login

--- app_selldrinks/lib/screens/home_screen.dart
+++ app_selldrinks/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:app_selldrinks/screens/homepage.dart';
+import 'package:app_selldrinks/screens/login_Screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,14 +21,9 @@ class _HomeScreenState extends State<HomeScreen> {
     super.didChangeDependencies();
     if (_isFirstBuild) {
       _page = [
+        HomePage(),
         Center(
-          child: Text(
-            'Cửa hàng',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        Center(
-          child: Text('Đặt hàng', style: Theme.of(context).textTheme.bodyLarge),
+          child: Text('----', style: Theme.of(context).textTheme.bodyLarge),
         ),
         Center(
           child: Text(
@@ -37,9 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
         Center(
           child: Text('Của hàng', style: Theme.of(context).textTheme.bodyLarge),
         ),
-        Center(
-          child: Text('Khác', style: Theme.of(context).textTheme.bodyLarge),
-        ),
+        LoginScreen(),
       ];
       _isFirstBuild = false;
     }
